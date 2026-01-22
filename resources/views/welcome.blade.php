56d0ccc0eeaf564d861b100cc49771cab6377088
thisathdewsen682 <thisathdewsen77@gmail.com>
2026-01-22 14:32:34 +0530

    Replace CDN fonts with npm package @fontsource/figtree

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,9 +9,6 @@
     <!-- Favicon -->
     <link rel="icon" type="image/svg+xml" href="{{ asset('favicon.svg') }}">
 
-    <link rel="preconnect" href="https://fonts.bunny.net">
-    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700&display=swap" rel="stylesheet" />
-
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
